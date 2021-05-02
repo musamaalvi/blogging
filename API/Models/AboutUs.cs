272ba885866638c3969a2a2455380d7fc62de2c2
M Usama Alvi <m.usamaalvi@gmail.com>
2021-05-02 22:06:57 +0500

    Integrating mongo db with dotnetcore api

---
+++ API/Models/AboutUs.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class AboutUs
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("aboutus")]
+        public string about { get; set; }
+
+        public DateTime date { get; set; }
+
+  
+    }
+}
