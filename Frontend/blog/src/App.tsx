835cae9d051c2359ed938779f2a460f3c54f47af
M Usama Alvi <m.usamaalvi@gmail.com>
2021-05-20 00:00:48 +0500

    changes

--- Frontend/blog/src/App.tsx
+++ Frontend/blog/src/App.tsx
@@ -2,21 +2,28 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import useAppState from './appState';
-
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  const {Counter, setCounter} = useAppState();
-  const handleClick = () => {
-    setCounter(Counter+1);
-  }
-  return (
-    <div className="App">
-      <h1>{Counter}</h1>
-     <button onClick={handleClick}>
-       increment counter
-     </button>
-    </div>
-  );
+  // const {Counter, setCounter} = useAppState();
+  // const handleClick = () => {
+  //   setCounter(Counter+1);
+  // }
+  // return (
+  //   <div className="App">
+  //     <h1>{Counter}</h1>
+  //    <button onClick={handleClick}>
+  //      increment counter
+  //    </button>
+  //   </div>
+  // );
+<Router>
+
+  
+</Router>
+
+
+
 }
 
 export default App;
